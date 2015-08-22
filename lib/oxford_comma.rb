606eeaa064a3e52a7cf76d3cfead818a3feaf612
NancyKo <konancys@gmail.com>
2015-08-22 13:08:15 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size <= 2 
+    array.join(" and ")
+  elsif array.size >= 3
+    last_element = array.last.prepend(", and ")
+    array.pop
+    array.join(", ") + last_element
+  end
 end
